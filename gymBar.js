c71c94ad86f2838037648d57833e6dde7cbe3cb3
mike-droid <miguelreyesmoreno@hotmail.com>
2025-03-09 20:58:04 -0500

    linting js file

--- gymBar.js
+++ gymBar.js
@@ -21,7 +21,7 @@ if (WebGL.isWebGL2Available()) {
 	const capsuleGeometry = new THREE.CapsuleGeometry(1.2, 50, 50, 50);
 	const capsuleMaterial = new THREE.MeshBasicMaterial({ color: 0xc6c6c6 });
 	const capsule = new THREE.Mesh(capsuleGeometry, capsuleMaterial);
-    capsule.rotation.z = Math.PI / 2; // Rotate the capsule to look horizontal
+	capsule.rotation.z = Math.PI / 2; // Rotate the capsule to look horizontal
 	barbellGroup.add(capsule);
 
 	// Create the first torus (weight plate)
@@ -29,7 +29,7 @@ if (WebGL.isWebGL2Available()) {
 	const torusMaterial1 = new THREE.MeshBasicMaterial({ color: 0xf90404 });
 	const torus1 = new THREE.Mesh(torusGeometry1, torusMaterial1);
 	torus1.position.x = 17; // Set position relative to the bar
-    torus1.rotation.y = Math.PI / 2; // Rotate the torus to look connected to the bar
+	torus1.rotation.y = Math.PI / 2; // Rotate the torus to look connected to the bar
 	barbellGroup.add(torus1);
 
 	// Create the second torus (weight plate)
@@ -37,7 +37,7 @@ if (WebGL.isWebGL2Available()) {
 	const torusMaterial2 = new THREE.MeshBasicMaterial({ color: 0xf90404 });
 	const torus2 = new THREE.Mesh(torusGeometry2, torusMaterial2);
 	torus2.position.x = -17; // Set position relative to the bar
-    torus2.rotation.y = Math.PI / 2; // Rotate the torus to look connected to the bar
+	torus2.rotation.y = Math.PI / 2; // Rotate the torus to look connected to the bar
 	barbellGroup.add(torus2);
 
 	// Add the entire barbell group to the scene
